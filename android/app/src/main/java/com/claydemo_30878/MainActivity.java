38506b815b29942be221373517fc09808a475a0b
Crowdbotics <team@crowdbotics.com>
2021-09-26 13:59:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/claydemo_30878/MainActivity.java
@@ -0,0 +1,15 @@
+package com.claydemo_30878;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "claydemo_30878";
+  }
+}
